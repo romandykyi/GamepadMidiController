654fba011a55c3cfffe6a0fb2e7b91dd65d24dea
Roman Dykyi <roman.dykyi.8210@gmail.com>
2026-06-28 20:43:06 +0200

    Use new architecture

--- GamepadMidiController/GamepadMidiController.cpp
+++ GamepadMidiController/GamepadMidiController.cpp
@@ -2,14 +2,6 @@
 
 std::optional<gamepad_midi::midi1_output> midi_out;
 
-const double DEADZONE = 0.3;
-const uint32_t DEFAULT_VELOCITY = 3381864012; // Velocity equal to 100
-
-static double humanize_velocity(uint32_t velocity)
-{
-	return round(static_cast<double>(velocity) / std::numeric_limits<uint32_t>::max() * 100) / 100;
-}
-
 static std::optional<libremidi::output_port> select_midi_out_port()
 {
 	std::cout << "Available MIDI output ports:\n";
@@ -37,11 +29,11 @@ static std::optional<libremidi::output_port> select_midi_out_port()
 		{
 			option = std::stoll(input);
 		}
-		catch (const std::invalid_argument& e)
+		catch (const std::invalid_argument&)
 		{
 			continue;
 		}
-		catch (const std::out_of_range& e)
+		catch (const std::out_of_range&)
 		{
 			continue;
 		}
@@ -54,142 +46,10 @@ static std::optional<libremidi::output_port> select_midi_out_port()
 	}
 }
 
-static void note_msg(float value, uint8_t channel, uint8_t note, uint32_t velocity)
-{
-	std::cout << "Note event: Channel: " << static_cast<int>(channel)
-		<< ", Note: " << static_cast<int>(note)
-		<< ", Velocity: " << humanize_velocity(velocity)
-		<< ", Value: " << value << '\n';
-	if (value < 0.5f)
-	{
-		midi_out->note_off(channel, note, velocity);
-	}
-	else
-	{
-		midi_out->note_on(channel, note, velocity);
-	}
-}
-
-static void button_handler(std::shared_ptr<gamepad::device> dev)
-{
-	auto event = dev->last_button_event();
-
-	switch (event->vc)
-	{
-	case gamepad::button::A:
-		note_msg(event->virtual_value, 1, 36, DEFAULT_VELOCITY);
-		break;
-	case gamepad::button::X:
-		note_msg(event->virtual_value, 1, 38, DEFAULT_VELOCITY);
-		break;
-	case gamepad::button::B:
-		note_msg(event->virtual_value, 1, 42, DEFAULT_VELOCITY);
-		break;
-	case gamepad::button::Y:
-		note_msg(event->virtual_value, 1, 46, DEFAULT_VELOCITY);
-		break;
-	default:
-		std::cout << "Button event: Native id: " << event->native_id
-			<< ", Virtual id: " << event->vc
-			<< ", V.Value: " << event->virtual_value << '\n';
-		break;
-	}
-}
-
-static void axis_cc(float value, uint8_t channel, uint8_t control)
-{
-	uint32_t midi_value = static_cast<uint32_t>(static_cast<double>(std::numeric_limits<uint32_t>::max()) * value);
-
-	midi_out->cc(channel, control, midi_value);
-}
-
-static void axis_handler(std::shared_ptr<gamepad::device> dev)
-{
-	auto event = dev->last_axis_event();
-
-	/*std::cout << "Axis event: Native id: " << event->native_id
-		<< ", Virtual id: " << event->vc
-		<< ", V.Value: " << event->virtual_value << '\n';*/
-
-	switch (event->vc)
-	{
-		case gamepad::axis::RIGHT_TRIGGER:
-			axis_cc(event->virtual_value, 2, 69);
-			break;
-		case gamepad::axis::LEFT_TRIGGER:
-			axis_cc(event->virtual_value, 2, 70);
-			break;
-	}
-}
-
-static void chord_msg(bool press, const std::vector<uint8_t>& notes, uint8_t channel, uint32_t velocity)
-{
-	std::cout << "Chord event: Channel: " << static_cast<int>(channel)
-		<< ", First Note: " << static_cast<int>(notes[0])
-		<< ", Velocity: " << humanize_velocity(velocity)
-		<< ", Action: " << (press ? "hold" : "release") << '\n';
-
-	for (uint8_t note : notes)
-	{
-		if (press)
-		{
-			midi_out->note_on(channel, note, velocity);
-		}
-		else
-		{
-			midi_out->note_off(channel, note, velocity);
-		}
-	}
-}
-
-static int values_to_index(float x, float y, int size)
-{
-	x = -x;
-	y = -y;
-
-	float magnitude = std::sqrt(x * x + y * y);
-	if (magnitude < DEADZONE)
-	{
-		return -1; // Outside of the deadzone
-	}
-
-	float angleRadians = std::atan2(y, x);
-	float angleDegrees = angleRadians * (180.0f / M_PI);
-
-	if (angleDegrees < 0)
-	{
-		angleDegrees += 360.0f;
-	}
-
-	// Normalize angle to 0 - 1
-	float value01 = angleDegrees / 360.0f;
-	value01 -= 0.25f; // Shift so 0 is at the top
-	value01 = value01 - floor(value01); // Ensure it's in the range [0, 1)
-
-	return (int)(value01 * size);
-}
-
-static void process_chords(const std::vector<std::vector<uint8_t>>& chord_notes, int& prev_index, float x, float y)
-{
-	int index = values_to_index(x, y, chord_notes.size());
-
-	if (index == prev_index) return;
-
-	if (prev_index != -1)
-	{
-		chord_msg(false, chord_notes[prev_index], 2, DEFAULT_VELOCITY);
-	}
-
-	if (index != -1)
-	{
-		chord_msg(true, chord_notes[index], 2, DEFAULT_VELOCITY);
-	}
-
-	prev_index = index;
-}
-
 int main()
 {
+	using namespace gamepad_midi;
+
 	std::optional<libremidi::output_port> out_port = select_midi_out_port();
 	if (!out_port.has_value())
 	{
@@ -199,58 +59,20 @@ int main()
 
 	std::cout << "\n";
 	std::cout << "Selected port: " << out_port->port_name << '\n';
-
+	
 	libremidi::midi_out midi;
 	midi.open_port(out_port.value());
-	midi_out.emplace(
-		gamepad_midi::midi1_output(midi)
-	);
 
-	auto h = gamepad::hook::make();
-	h->set_plug_and_play(true, gamepad::ms(1000));
+	midi1_output midi_output(midi);
+	g2m_processor<midi1_output> processor(midi_output);
+	input_handler<g2m_processor<midi1_output>> gamepad_input(processor);
 
-	h->set_axis_event_handler(axis_handler);
-	h->set_button_event_handler(button_handler);
-
-	if (!h->start()) 
-	{
-		std::cerr << "Failed to start hook\n";
-		return 1;
-	}
-
-	int l_prev_index = -1;
-	int r_prev_index = -1;
-	std::vector<std::vector<uint8_t>> left_notes = 
-	{
-		{ 61, },
-		{ 62, },
-		{ 64, },
-		{ 65, },
-		{ 67, },
-		{ 69, },
-	};
-	std::vector<std::vector<uint8_t>> right_notes =
-	{
-		{ 61 + 12, },
-		{ 62 + 12, },
-		{ 64 + 12, },
-		{ 65 + 12, },
-		{ 67 + 12, },
-		{ 69 + 12, },
-	};
+	gamepad_input.start_listening();
+	std::cout << "Listening for input..." << std::endl;
 	while (true)
 	{
-		float lx, ly;
-		lx = h->get_devices()[0]->get_axis(gamepad::axis::LEFT_STICK_X);
-		ly = h->get_devices()[0]->get_axis(gamepad::axis::LEFT_STICK_Y);
-
-		process_chords(left_notes, l_prev_index, lx, ly);
-
-		float rx, ry;
-		rx = h->get_devices()[0]->get_axis(gamepad::axis::RIGHT_STICK_X);
-		ry = h->get_devices()[0]->get_axis(gamepad::axis::RIGHT_STICK_Y);
-
-		process_chords(right_notes, r_prev_index, rx, ry);
+		gamepad_input.poll();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
 
 	return 0;
