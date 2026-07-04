d6ab8c4e054351a9a5adb04ccc20bdd789307e38
Roman Dykyi <roman.dykyi.8210@gmail.com>
2026-07-04 17:33:43 +0200

    Add documentation for the MIDI component

--- GamepadMidiController/output/midi1_output.h
+++ GamepadMidiController/output/midi1_output.h
@@ -7,16 +7,45 @@
 
 namespace gamepad_midi
 {
+    /**
+     * @brief A component that is responsible for sending MIDI 1 events.
+     */
     class midi1_output
     {
     private:
         libremidi::midi_out& _midi;    
 
     public:
+        /**
+         * @brief Creates an instance based on the libremidi out port.
+         * 
+         * @param midi A libremidi out port. Must be initialized and opened before. Must outlive this class.
+         */
         midi1_output(libremidi::midi_out& midi);
 
+        /**
+         * @brief Sends the MIDI note on event.
+         * 
+         * @param channel A MIDI channel to use.
+         * @param note A MIDI note to be sent.
+         * @param velocity The note's velocity.
+         */
         void note_on(uint8_t channel, uint8_t note, uint32_t velocity);
+        /**
+         * @brief Sends the MIDI note off event.
+         * 
+         * @param channel A MIDI channel to use.
+         * @param note A MIDI note to be sent.
+         * @param velocity The note's velocity.
+         */
         void note_off(uint8_t channel, uint8_t note, uint32_t velocity);
+        /**
+         * @brief Sends the MIDI CC event.
+         * 
+         * @param channel A channel to use.
+         * @param control A MIDI control to be sent.
+         * @param value The control's value.
+         */
         void cc(uint8_t channel, uint8_t control, uint32_t value);
     };
 }
