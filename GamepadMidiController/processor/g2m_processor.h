78b1165db4bc0235523e412291cc5df1b31fc20f
Roman Dykyi <roman.dykyi.8210@gmail.com>
2026-07-04 17:51:31 +0200

    Add documentation for the processor component

--- GamepadMidiController/processor/g2m_processor.h
+++ GamepadMidiController/processor/g2m_processor.h
@@ -8,12 +8,36 @@
 
 namespace gamepad_midi
 {
-    constexpr uint8_t SKIP_NOTE = std::numeric_limits<uint8_t>::max();
-    constexpr uint8_t SKIP_CONTROL = std::numeric_limits<uint8_t>::max();
-    constexpr uint32_t DEFAULT_VELOCITY = 3381864012;
-    constexpr float JOYSTICK_DEADZONE = 0.3f; // TODO: replace with config
-    constexpr int32_t AXIS_DEADZONE = 100; // TODO: replace with config (?)
-    constexpr int32_t AXIS_CHANGE_SENSITIVITY = 100; // TODO: replace with config (?)
+    /**
+     * @brief A note code that makes the processor ignore the button.
+     */
+    inline constexpr uint8_t SKIP_NOTE = std::numeric_limits<uint8_t>::max();
+    /**
+     * @brief A control code that makes the processor ignore the axis.
+     */
+    inline constexpr uint8_t SKIP_CONTROL = std::numeric_limits<uint8_t>::max();
+    /**
+     * @brief The default velocity (equals 100 in MIDI 1).
+     */
+    inline constexpr uint32_t DEFAULT_VELOCITY = 3381864012;
+    /**
+     * @brief The joystick deadzone where notes are not registered.
+     * 
+     * Will be replaced with a config.
+     */
+    inline constexpr float JOYSTICK_DEADZONE = 0.3f; // TODO: replace with config
+    /**
+     * @brief The axis deadzone for CC events.
+     * 
+     * Will be replaced with a config.
+     */
+    inline constexpr int32_t AXIS_DEADZONE = 100; // TODO: replace with config (?)
+    /**
+     * @brief The axis change sensitivity threshold. Changes under this threshold are not registered.
+     * 
+     * Will be replaced with a config.
+     */
+    inline constexpr int32_t AXIS_CHANGE_SENSITIVITY = 100; // TODO: replace with config (?)
 
     /**
      * @brief Requires support for sending MIDI note on event.
@@ -60,6 +84,11 @@ namespace gamepad_midi
         has_note_off<t_midi_out> &&
         has_cc<t_midi_out>;
 
+    /**
+     * @brief A component responsible for mapping gamepad input into MIDI output.
+     * 
+     * @tparam midi_out A template for a component that sends MIDI events.
+     */
     template <midi_out t_midi_out>
     class g2m_processor
     {    
@@ -79,10 +108,30 @@ namespace gamepad_midi
         void play_joystick_chord(const std::vector<std::vector<uint8_t>>& chord_notes, int& prev_index, int32_t x, int32_t y);
 
     public:
+        /**
+         * @brief Creates an instance based on the MIDI sender.
+         * 
+         * @param midi_out The MIDI event sender. Must outlive this class.
+         */
         g2m_processor(t_midi_out midi_out);
 
+        /**
+         * @brief Processes the gamepad button events (pressed/released).
+         * 
+         * @param button_code The gamepad button that was pressed/released.
+         * @param button_state The state of the gamepad button (pressed/released).
+         */
         void on_button_event(gamepad_button button_code, button_state button_state);
+        /**
+         * @brief Processes the gamepad axis event.
+         * 
+         * @param axis_code The gamepad axis that is being moved.
+         * @param axis_value The updated axis value. Assumed to never be negative for triggers.
+         */
         void on_axis_event(gamepad_axis axis_code, int32_t axis_value);
+        /**
+         * Processes the end of the input polling cycle.
+         */
         void on_poll_end(void);
     };
 }
