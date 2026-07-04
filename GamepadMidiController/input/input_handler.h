cfa3c8842606c5e5e420cf0821de2372bf9466ca
Roman Dykyi <roman.dykyi.8210@gmail.com>
2026-07-04 16:54:45 +0200

    Document concepts

--- GamepadMidiController/input/input_handler.h
+++ GamepadMidiController/input/input_handler.h
@@ -7,19 +7,45 @@
 #include "gamepad_scheme.h"
 
 namespace gamepad_midi
-{    
-    template<typename t_receiver>
-    concept input_receiver = requires(
-        t_receiver r, 
-        gamepad_button button_code, 
-        button_state button_state, 
-        gamepad_axis axis_code,
-        int32_t axis_value
-    ) {
-        { r.on_button_event(button_code, button_state) } -> std::same_as<void>;
-        { r.on_axis_event(axis_code, axis_value) } -> std::same_as<void>;
+{
+    /**
+     * @brief Requires support for receiving button presses and releases.
+     * 
+     * @param button The gamepad button that was pressed/released.
+     * @param state The state of the gamepad button (pressed/released).
+     */
+    template<typename T>
+    concept has_on_button_event = requires(T r, gamepad_button button, button_state state) {
+        { r.on_button_event(button, state) } -> std::same_as<void>;
+    };
+
+    /**
+     * @brief Requires support for receiving analog axis movements.
+     * 
+     * @param axis The gamepad axis that is being moved.
+     * @param value The updated axis value. Never negative for triggers.
+     */
+    template<typename T>
+    concept has_on_axis_event = requires(T r, gamepad_axis axis, int32_t value) {
+        { r.on_axis_event(axis, value) } -> std::same_as<void>;
+    };
+
+    /**
+     * @brief Requires a notification callback for the end of the input polling cycle.
+     */
+    template<typename T>
+    concept has_on_poll_end = requires(T r) {
         { r.on_poll_end() } -> std::same_as<void>;
     };
+
+    /**
+     * @brief A concept for a component that receives input from a gamepad.
+     */
+    template<typename t_receiver>
+    concept input_receiver = 
+        has_on_button_event<t_receiver> && 
+        has_on_axis_event<t_receiver> && 
+        has_on_poll_end<t_receiver>;
 
     template <input_receiver t_receiver>
     class input_handler
@@ -40,7 +66,6 @@ namespace gamepad_midi
         void start_listening(void);
         void poll(void);
     };
-
 }
 
 #include "input_handler.tpp"
